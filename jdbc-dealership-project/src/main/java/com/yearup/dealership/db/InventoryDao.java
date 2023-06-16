3602b4ed9907f4045ebf4cdb5e214af2603ad41a
Nadav Dayan <ndayan@appdev.yearup.org>
2023-06-16 11:15:34 -0400

    InventoryDao methods are coded

--- jdbc-dealership-project/src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ jdbc-dealership-project/src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -14,9 +14,40 @@ public class InventoryDao {
 
     public void addVehicleToInventory(String vin, int dealershipId) {
         // TODO: Implement the logic to add a vehicle to the inventory
+
+        String sql = "INSERT INTO inventory VALUES (?,?)";
+        int rows = 0;
+
+        try (Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+            preparedStatement.setString(1,vin);
+            preparedStatement.setInt(2, dealershipId);
+
+            rows = preparedStatement.executeUpdate();
+            System.out.println(rows + " row(s) updated!");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void removeVehicleFromInventory(String vin) {
         // TODO: Implement the logic to remove a vehicle from the inventory
+
+        String sql = "DELETE FROM inventory WHEN VIN = ?";
+        int rows = 0;
+
+        try (Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setString(1,vin);
+
+            rows = preparedStatement.executeUpdate();
+            System.out.println(rows + " row(s) updated!");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
